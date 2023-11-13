b2bdb0bf819fe5c77a03a5d0a92a16499fe2acd1
baikhsam <baikhsam@gmail.com>
2023-11-13 17:47:19 -0500

    add dotenv

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ import Lab5 from "./Lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
+import "dotenv/config";
+import session from "express-session";
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+}));
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
 Hello(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
