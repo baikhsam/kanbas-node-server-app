2c311ddf24f203b7d603f15115c8dacf83c21be3
baikhsam <baikhsam@gmail.com>
2023-11-17 22:22:54 -0500

    add assignment routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import Hello from "./hello.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import AssignmentRoutes from "./assignments/routes.js";
 import cors from "cors";
 import "dotenv/config";
 // import session from "express-session";
@@ -15,6 +16,7 @@ app.use(cors({
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
+AssignmentRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
