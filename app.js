d453b0c9732096cc3db247914ada8a4e65695cd1
baikhsam <baikhsam@gmail.com>
2023-11-30 18:32:49 -0500

    add users CRU operations

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import cors from "cors";
 import "dotenv/config";
 // import session from "express-session";
 import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
 mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 const app = express();
@@ -18,6 +19,7 @@ app.use(
 	})
 );
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 AssignmentRoutes(app);
